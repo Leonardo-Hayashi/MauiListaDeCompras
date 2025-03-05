6b1f8c9bce6867b026ab065068d2096ddf9b8048
Leonardo Hayashi <leonardodearaujohay@gmail.com>
2025-03-05 19:46:17 -0300

    tela de listar produtos, tela de adicionar produtos, ambas configuradas

--- MauiListaDeCompras/App.xaml.cs
+++ MauiListaDeCompras/App.xaml.cs
@@ -1,7 +1,32 @@
-﻿namespace MauiListaDeCompras
+﻿using MauiListaDeCompras.Helpers;
+
+namespace MauiListaDeCompras
 {
     public partial class App : Application
     {
+        
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+        
+        
         public App()
         {
             InitializeComponent();
--- MauiListaDeCompras/Views/NovoProduto.xaml.cs
+++ MauiListaDeCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiListaDeCompras.Models;
+
 namespace MauiListaDeCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,24 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao  = txt_Descricao.Text,
+				Quantidade  = Convert.ToDouble(txt_Quantidade.Text),
+				Preco = Convert.ToDouble(txt_Preco.Text)
+			};
+			
+			await App.Db.Insert(p);
+			await DisplayAlert("Sucesso!", "Registro Inserido", "OK"); 
+
+		} catch(Exception ex)
+		{
+            DisplayAlert("Oops", ex.Message, "OK");
+        }
+    }
 }
